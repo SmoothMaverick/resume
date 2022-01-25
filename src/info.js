308d6197d9af0d14b50033e33566f7ac9ede1443
jason <5660582+SmoothMaverick@users.noreply.github.com>
2022-01-25 11:19:09 -0800

    Fix formatting for eslint

--- src/info.js
+++ src/info.js
@@ -1,20 +1,20 @@
-const contact = {
-  "name": "Jason Hoang",
-  "email": "jason.hoang@haighten.com",
-  "linkedin": "linkedin.com/in/the-jason-hoang",
-  "github": "github.com/SmoothMaverick"
+export const contact = {
+  'name': 'Jason Hoang',
+  'email': 'jason.hoang@haighten.com',
+  'linkedin': 'linkedin.com/in/the-jason-hoang',
+  'github': 'github.com/SmoothMaverick'
 };
 
-const education = {
-  "school": "University of California, Los Angeles (UCLA)",
-  "degree": "Bachelor of Science in Microbiology, 2007"
+export const education = {
+  'school': 'University of California, Los Angeles (UCLA)',
+  'degree': 'Bachelor of Science in Microbiology, 2007'
 };
 
-const headline = "Aspiring to solve challenges to grow businesses using software and creative solutions.";
+export const headline = 'Aspiring to solve challenges to grow businesses using software and creative solutions.';
 
-const skills = [
-  "Languages: Python, Javascript, Swift",
-  "Cloud: AWS, Google Cloud",
-  "Configuration Management: Terraform, Puppet, Chef",
-  "Operating systems: Linux, iOS",
-]
+export const skills = [
+  'Languages: Python, Javascript, Swift',
+  'Cloud: AWS, Google Cloud',
+  'Configuration Management: Terraform, Puppet, Chef',
+  'Operating systems: Linux, iOS',
+];
--- src/timeline.js
+++ src/timeline.js
@@ -1,170 +1,170 @@
-const timeline = [
-    {
-        "title": "Capital Technology Group",
-        "subtitle": "Systems Developer",
-        "detail": "Washington, DC (remote)",
-        "startDate": "Dec 2021",
-        "endDate": "present",
-        "faIcon": "fa-asterisk",
-        "summary": [
-            "Building out a frontend React application for the Department of Homeland Security",
-        ]
-    },
-    {
-        "title": "Copado",
-        "subtitle": "Senior DevOps Engineer (contract)",
-        "detail": "San Francisco, CA",
-        "startDate": "Mar 2019",
-        "endDate": "present",
-        "faIcon": "fa-asterisk",
-        "summary": [
-            "Joined as an independent contractor, worked on as needed basis.",
-            "Created an integration with Hashicorp's Vault and Venafi's Trusted Protection Platform.",
-            "Build services and infrastructure for applications running in Kubernetes",
-            "Created tooling to allow secrets (e.g. passwords, certificates) to be easily shared between the two systems.",
-            "Apply Lean Security principles to support client engagements in infrastructure automation and security architecture.",
-        ]
-    },
-    {
-        "title": "Intermission",
-        "subtitle": "",
-        "detail": "",
-        "startDate": "Jun 2020",
-        "endDate": "Dec 2021",
-        "faIcon": "fa-asterisk",
-        "summary": [
-            "Took a gap year and bunkered down for Covid-19.",
-            "Used the time to think about the next step in my career, study for interviews, and spend time with family.",
-            "Learned iOS development and released an app to the App Store.",
-        ]
-    },
-    {
-        "title": "Haighten, LLC",
-        "subtitle": "Software Engineer",
-        "detail": "San Francisco, CA",
-        "startDate": "Jun 2019",
-        "endDate": "present",
-        "faIcon": "fa-asterisk",
-        "summary": [
-            "Personal LLC for contracting.",
-        ]
-    },
-    {
-        "title": "Ouster",
-        "subtitle": "Cloud Engineer (contract)",
-        "detail": "San Francisco, CA",
-        "startDate": "Jun 2019",
-        "endDate": "Apr 2020",
-        "faIcon": "fa-asterisk",
-        "summary": [
-            "Joined Ouster to migrate applications to Kubernetes and build the CICD pipeline.",
-            "Created a scalable simulator for the sensor software.",
-            "Deployed as a Kubernetes pod, this would process pre-loaded data, push to the cloud and act as a moving vehicle on the web application.",
-            "Support cloud services such as Kubernetes, App Engine, Big Query, Storage, IoT, Dataflow and Cloud Functions.",
-            "Provide web application support running on Flask and Python.",
-            "Improve deployments using Helm templates for Kubernetes.",
-            "Implement infrastructure-as-code with Terraform."
-        ]
-    },
-    {
-        "title": "Kony, Inc",
-        "subtitle": "Software Development Engineer",
-        "detail": "Menlo Park, CA",
-        "startDate": "Oct 2018",
-        "endDate": "Jun 2019",
-        "faIcon": "fa-usd",
-        "summary": [
-            "Continued efforts from Pivotus Ventures after the acquisition.",
-            "Setup Spinnaker to coordinate disjointed jobs and perform production deployments with a push button.",
-            "Continued improving the infrastructure after the acquisition.",
-            "Trained a team of 3 in Hyderabad, India to provide remote technical support and perform deployments.",
-            "Setup Helm and created charts so that deployed Kubernetes resources are consistent across environments."
-        ]
-    },
-    {
-        "title": "Pivotus Ventures (acquired by Kony)",
-        "subtitle": "DevOps Engineer",
-        "detail": "Menlo Park, CA",
-        "startDate": "Feb 2018",
-        "endDate": "Oct 2018",
-        "faIcon": "fa-address-book",
-        "summary": [
-            "Joined Pivotus to build scale their MVP to a global product.",
-            "Lead infrastructure-as-code practices with a team that preferred shells scripts and manual proceseses.",
-            "Brought consistency and ease of managing more than 10 environments across 3 geographic regions.",
-            "Created re-usable Terraform modules for AWS services such as S3, VPC, EC2, EKS, RDS, Elasticache and Lambda functions.",
-            "Created tools written in Golang to perform repetitive functions such as checking deployed artifacts for nine projects by their hash values.",
-            "Implemented monitoring, logging and security tools unique to applications deployed on Kubernetes."
-        ]
-    },
-    {
-        "title": "Education Elements",
-        "subtitle": "DevOps Engineer",
-        "detail": "San Carlos, CA",
-        "startDate": "Dec 2016",
-        "endDate": "Oct 2017",
-        "faIcon": "fa-graduation-cap",
-        "summary": [
-            "Joined Education Elements in my first role as the lead DevOps Engineer.",
-            "Sole engineer responsible for the entire infrastructure, reported under the VP of Engineering.",
-            "Migrated applications deployed on a host server to pods in Kubernetes.",
-            "Setup Prometheus for monitoring Kubernetes services.",
-            "Developed an on-demand deployment system to increase development speed annd reduce AWS costs.",
-            "Reduced AWS costs by 33% within the first 4 months.",
-            "Managed servers with Salt and Chef."
-        ]
-    },
-    {
-        "title": "Itson, Inc.",
-        "subtitle": "DevOps Engineer",
-        "detail": "Redwood City, CA",
-        "startDate": "May 2015",
-        "endDate": "Nov 2016",
-        "faIcon": "fa-mobile",
-        "summary": [
-            "Joined Itson to scale their MVP to a mature product.",
-            "Managed deployment of our initial saas infrastructure to production, partnered with large telecom carriers (Sprint, Telefonica, Sapphire).",
-            "Reduced time to run build-verification by running builds in parallel and using docker to deploy.",
-            "Simplified deployment of AWS stack using Cloudformation/Terraform."
-        ]
-    },
-    {
-        "title": "Podomatic, Inc",
-        "subtitle": "Software Engineer",
-        "detail": "San Francisco, CA",
-        "startDate": "March 2014",
-        "endDate": "May 2015",
-        "faIcon": "fa-music",
-        "summary": [
-            "Started as an intern and was offered a job as a software engineer.",
-            "Created a unified development environment with Vagrant and Puppet.",
-            "Optimized site design using A/B testing with Google Analytics.",
-            "Improved server automation by translating scripts Puppet modules.",
-            "Backend Ruby on Rails development."
-        ]
-    },
-    {
-        "title": "City College of San Francisco",
-        "subtitle": "Assistant Network Technician",
-        "detail": "San Francisco, CA",
-        "startDate": "June 2013",
-        "endDate": "May 2014",
-        "faIcon": "fa-university",
-        "summary": [
-            "Resolved issues with Wifi connections in a timely, professional manner.",
-            "Performed quality control and testing for deployment of latest Meraki WiFi Technology."
-        ]
-    },
-    {
-        "title": "US Navy",
-        "subtitle": "Naval Officer",
-        "detail": "various",
-        "startDate": "Feb 2008",
-        "endDate": "Jul 2012",
-        "faIcon": "fa-ship",
-        "summary": [
-            "Volunteered to become an advisor for the Afghan National Civil Order Police (ANCOP), working with their communications officer and intelligence officer in Kandahar, Afghanistan.",
-            "Maintained deployment readiness with the San Jose reserve unit.",
-        ]
-    }
+export const timeline = [
+  {
+    'title': 'Capital Technology Group',
+    'subtitle': 'Systems Developer',
+    'detail': 'Washington, DC (remote)',
+    'startDate': 'Dec 2021',
+    'endDate': 'present',
+    'faIcon': 'fa-asterisk',
+    'summary': [
+      'Building out a frontend React application for the Department of Homeland Security',
+    ]
+  },
+  {
+    'title': 'Copado',
+    'subtitle': 'Senior DevOps Engineer (contract)',
+    'detail': 'San Francisco, CA',
+    'startDate': 'Mar 2019',
+    'endDate': 'present',
+    'faIcon': 'fa-asterisk',
+    'summary': [
+      'Joined as an independent contractor, worked on as needed basis.',
+      'Created an integration with Hashicorp\'s Vault and Venafi\'s Trusted Protection Platform.',
+      'Build services and infrastructure for applications running in Kubernetes',
+      'Created tooling to allow secrets (e.g. passwords, certificates) to be easily shared between the two systems.',
+      'Apply Lean Security principles to support client engagements in infrastructure automation and security architecture.',
+    ]
+  },
+  {
+    'title': 'Intermission',
+    'subtitle': '',
+    'detail': '',
+    'startDate': 'Jun 2020',
+    'endDate': 'Dec 2021',
+    'faIcon': 'fa-asterisk',
+    'summary': [
+      'Took a gap year and bunkered down for Covid-19.',
+      'Used the time to think about the next step in my career, study for interviews, and spend time with family.',
+      'Learned iOS development and released an app to the App Store.',
+    ]
+  },
+  {
+    'title': 'Haighten, LLC',
+    'subtitle': 'Software Engineer',
+    'detail': 'San Francisco, CA',
+    'startDate': 'Jun 2019',
+    'endDate': 'present',
+    'faIcon': 'fa-asterisk',
+    'summary': [
+      'Personal LLC for contracting.',
+    ]
+  },
+  {
+    'title': 'Ouster',
+    'subtitle': 'Cloud Engineer (contract)',
+    'detail': 'San Francisco, CA',
+    'startDate': 'Jun 2019',
+    'endDate': 'Apr 2020',
+    'faIcon': 'fa-asterisk',
+    'summary': [
+      'Joined Ouster to migrate applications to Kubernetes and build the CICD pipeline.',
+      'Created a scalable simulator for the sensor software.',
+      'Deployed as a Kubernetes pod, this would process pre-loaded data, push to the cloud and act as a moving vehicle on the web application.',
+      'Support cloud services such as Kubernetes, App Engine, Big Query, Storage, IoT, Dataflow and Cloud Functions.',
+      'Provide web application support running on Flask and Python.',
+      'Improve deployments using Helm templates for Kubernetes.',
+      'Implement infrastructure-as-code with Terraform.'
+    ]
+  },
+  {
+    'title': 'Kony, Inc',
+    'subtitle': 'Software Development Engineer',
+    'detail': 'Menlo Park, CA',
+    'startDate': 'Oct 2018',
+    'endDate': 'Jun 2019',
+    'faIcon': 'fa-usd',
+    'summary': [
+      'Continued efforts from Pivotus Ventures after the acquisition.',
+      'Setup Spinnaker to coordinate disjointed jobs and perform production deployments with a push button.',
+      'Continued improving the infrastructure after the acquisition.',
+      'Trained a team of 3 in Hyderabad, India to provide remote technical support and perform deployments.',
+      'Setup Helm and created charts so that deployed Kubernetes resources are consistent across environments.'
+    ]
+  },
+  {
+    'title': 'Pivotus Ventures (acquired by Kony)',
+    'subtitle': 'DevOps Engineer',
+    'detail': 'Menlo Park, CA',
+    'startDate': 'Feb 2018',
+    'endDate': 'Oct 2018',
+    'faIcon': 'fa-address-book',
+    'summary': [
+      'Joined Pivotus to build scale their MVP to a global product.',
+      'Lead infrastructure-as-code practices with a team that preferred shells scripts and manual proceseses.',
+      'Brought consistency and ease of managing more than 10 environments across 3 geographic regions.',
+      'Created re-usable Terraform modules for AWS services such as S3, VPC, EC2, EKS, RDS, Elasticache and Lambda functions.',
+      'Created tools written in Golang to perform repetitive functions such as checking deployed artifacts for nine projects by their hash values.',
+      'Implemented monitoring, logging and security tools unique to applications deployed on Kubernetes.'
+    ]
+  },
+  {
+    'title': 'Education Elements',
+    'subtitle': 'DevOps Engineer',
+    'detail': 'San Carlos, CA',
+    'startDate': 'Dec 2016',
+    'endDate': 'Oct 2017',
+    'faIcon': 'fa-graduation-cap',
+    'summary': [
+      'Joined Education Elements in my first role as the lead DevOps Engineer.',
+      'Sole engineer responsible for the entire infrastructure, reported under the VP of Engineering.',
+      'Migrated applications deployed on a host server to pods in Kubernetes.',
+      'Setup Prometheus for monitoring Kubernetes services.',
+      'Developed an on-demand deployment system to increase development speed annd reduce AWS costs.',
+      'Reduced AWS costs by 33% within the first 4 months.',
+      'Managed servers with Salt and Chef.'
+    ]
+  },
+  {
+    'title': 'Itson, Inc.',
+    'subtitle': 'DevOps Engineer',
+    'detail': 'Redwood City, CA',
+    'startDate': 'May 2015',
+    'endDate': 'Nov 2016',
+    'faIcon': 'fa-mobile',
+    'summary': [
+      'Joined Itson to scale their MVP to a mature product.',
+      'Managed deployment of our initial saas infrastructure to production, partnered with large telecom carriers (Sprint, Telefonica, Sapphire).',
+      'Reduced time to run build-verification by running builds in parallel and using docker to deploy.',
+      'Simplified deployment of AWS stack using Cloudformation/Terraform.'
+    ]
+  },
+  {
+    'title': 'Podomatic, Inc',
+    'subtitle': 'Software Engineer',
+    'detail': 'San Francisco, CA',
+    'startDate': 'March 2014',
+    'endDate': 'May 2015',
+    'faIcon': 'fa-music',
+    'summary': [
+      'Started as an intern and was offered a job as a software engineer.',
+      'Created a unified development environment with Vagrant and Puppet.',
+      'Optimized site design using A/B testing with Google Analytics.',
+      'Improved server automation by translating scripts Puppet modules.',
+      'Backend Ruby on Rails development.'
+    ]
+  },
+  {
+    'title': 'City College of San Francisco',
+    'subtitle': 'Assistant Network Technician',
+    'detail': 'San Francisco, CA',
+    'startDate': 'June 2013',
+    'endDate': 'May 2014',
+    'faIcon': 'fa-university',
+    'summary': [
+      'Resolved issues with Wifi connections in a timely, professional manner.',
+      'Performed quality control and testing for deployment of latest Meraki WiFi Technology.'
+    ]
+  },
+  {
+    'title': 'US Navy',
+    'subtitle': 'Naval Officer',
+    'detail': 'various',
+    'startDate': 'Feb 2008',
+    'endDate': 'Jul 2012',
+    'faIcon': 'fa-ship',
+    'summary': [
+      'Volunteered to become an advisor for the Afghan National Civil Order Police (ANCOP), working with their communications officer and intelligence officer in Kandahar, Afghanistan.',
+      'Maintained deployment readiness with the San Jose reserve unit.',
+    ]
+  }
 ];
